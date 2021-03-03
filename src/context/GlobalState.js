529cd6102e29a199dfcfbeba1f5558533784d8f1
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-03 10:22:22 -0300

    persisting data to localstorage

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,10 +1,14 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
 // initial state
 const initialState = {
-  watchlist: [],
-  watched: [],
+  watchlist: localStorage.getItem('watchlist')
+    ? JSON.parse(localStorage.getItem('watchlist'))
+    : [],
+  watched: localStorage.getItem('watched')
+    ? JSON.parse(localStorage.getItem('watched'))
+    : [],
 };
 
 //create context
@@ -13,6 +17,11 @@ export const GlobalContext = createContext(initialState);
 //provider component
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+    localStorage.setItem('watched', JSON.stringify(state.watched));
+  }, [state]);
 
   //actions
   const addMovieToWatchlist = (movie) => {
