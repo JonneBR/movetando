c6bd978dc68e2def423ae5788ecae6e498382c4f
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-21 11:39:58 -0300

    removing console log test

--- src/components/Add.js
+++ src/components/Add.js
@@ -8,7 +8,7 @@ export const Add = () => {
   const onChange = (e) => {
     e.preventDefault();
     setQuery(e.target.value);
-    console.log('commented');
+
     fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
