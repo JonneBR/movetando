0a53c3e62f6cb8725b084fe0b174f23380a24e40
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-21 11:23:01 -0300

    saving changes

--- src/components/Add.js
+++ src/components/Add.js
@@ -10,9 +10,7 @@ export const Add = () => {
   const onChange = (e) => {
     e.preventDefault();
     setQuery(e.target.value);
-<<<<<<< HEAD
     console.log(e.target.value);
-=======
 
     fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
@@ -25,7 +23,6 @@ export const Add = () => {
           setResults([]);
         }
       });
->>>>>>> jsbranch
   };
   return (
     <div className='add-page'>
